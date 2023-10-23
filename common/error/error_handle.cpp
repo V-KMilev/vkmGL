783677393fb706dd1aee018696b83a4e3e08d532
V-KMilev <vilimir.k.milev@gmail.com>
2023-10-24 00:37:49 +0300

    Error(assert) print change

--- common/error/error_handle.cpp
+++ common/error/error_handle.cpp
@@ -1,27 +1,19 @@
 #include "error_handle.h"
 
-#include <iostream>
+#include <stdio.h>
 
 void __m_assert(
-	const char* expected_str,
 	bool expected,
+	const char* expected_str,
 	const char* file,
 	int line,
-	const char* function,
 	const char* message
 ) {
 	if (expected) {
 		return;
 	}
 
-	if(message) {
-		std::cerr << "M_ASSERT FAILED: "
-			<< function << " [ERROR::" << expected_str << "] at [" << file << ":" << line << "]\n"
-			<< "Message: " << message << "\n";
-	} else {
-		std::cerr << "M_ASSERT FAILED: "
-			<< function << " [ERROR::" << expected_str << "] at [" << file << ":" << line << "]\n"; 
-	}
+	printf("[FAILED] M_ASSERT >>\t[%s:%d] Expected: '%s', Info: %s\n", file, line, expected_str, message);
 
 #ifdef _WIN32
 		__debugbreak();
--- common/error/error_handle.h
+++ common/error/error_handle.h
@@ -1,36 +1,12 @@
 #pragma once
 
-/**
- * @brief Private function called only by the macro 
- * m_assert used for printing the assert
- * 
- * @note Assert print is been called only if the given 
- * expectation is wrong. There are two print formats. 
- * The first is without a comment message and the 
- * second is with comment message. __FILE__, __LINE__ 
- * and __FUNCTION__ are equal of the file, line and 
- * funtion the M_ASSERT is being executed. __VA_ARGS__ 
- * is the representation of the comment message 
- * 
- * @param[in] expected_str 
- * @param[in] expected 
- * @param[in] file 
- * @param[in] line 
- * @param[in] function 
- * @param[in] message 
- */
 void __m_assert(
-	const char* expected_str,
 	bool expected,
+	const char* expected_str,
 	const char* file,
 	int line,
-	const char* function,
 	const char* message = nullptr
 );
 
-#ifdef NDEBUG
-	#define M_ASSERT(Expected, ...)
-#else
-	#define M_ASSERT(Expected, ...)                                                     \
-		__m_assert(#Expected, Expected, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__)
-#endif
+#define M_ASSERT(Expected, ...)                                      \
+	__m_assert(Expected, #Expected, __FILE__, __LINE__, ##__VA_ARGS__)
